f2e8d8117f37a2c4935263f6ad6f5e1213f5e4b3
Juan Diego Perez Martinez <juanperezdz@unicauca.edu.co>
2025-11-12 00:26:42 -0500

    Añadir nueva carpeta al repositorio

--- Proyecto Segundo Corte/Front/frontProyecto/src/app/templates/client-template/client-template.component.ts
+++ Proyecto Segundo Corte/Front/frontProyecto/src/app/templates/client-template/client-template.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { HeaderComponent } from '../../shared/organisms/header/header.component';
 
 @Component({
@@ -9,5 +9,4 @@ import { HeaderComponent } from '../../shared/organisms/header/header.component'
   styleUrl: './client-template.component.css'
 })
 export class ClientTemplateComponent {
-
 }
